3d7ab33336385d1e4d7a12130295ef6696354207
Sagar Pednekar <sagarpednekar04@gmail.com>
2024-10-17 15:46:15 +0530

    feat: Add WorkExperience component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import Testimonials from "@/components/Testimonials";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import WorkExperience from "@/components/WorkExperience";
 import { navItems } from "@/data";
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Testimonials />
+        < WorkExperience />
       </div>
     </main>
   );
